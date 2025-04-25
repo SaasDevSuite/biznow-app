a8064a91a278b499a4cb8d38a0ae98384a50e7b2
Priyankara2003 <srinathpriyankara77@gmail.com>
2025-04-25 16:08:58 +0530

    chore : update cron job for processing news.

--- cron.ts
+++ cron.ts
@@ -1,15 +1,21 @@
 import cron from 'node-cron';
 import { scrapeAndStoreNews } from './src/service/scraper.js';
+import dotenv from 'dotenv';
+import { processAllNews } from "./src/actions/news/query.js";
+dotenv.config();
 
-cron.schedule("* * * * *", async () => {
-    console.log("Running the scraping job...");
+cron.schedule("*/5 * * * *", async () => {
+    console.log("💻💻 Running the scraping job...💻💻");
 
     try {
-        await scrapeAndStoreNews();
-        console.log("News scraping job completed!");
+        await scrapeAndStoreNews().then(async () => {
+            console.log("🏁🏁 News scraped!!! now processing........! 🏁🏁");
+            await processAllNews();
+        });
+        console.log("🎉🎉 News scraping job completed! 🎉🎉");
     } catch (error) {
         console.error("Cron job error:", error);
     }
 });
 
-console.log("Running the scraping job...");
+console.log("Running the scraping job... executed.");
